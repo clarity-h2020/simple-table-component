cc551d34353eb68df7e1832f79a3f533f3e7fe96
p-a-s-c-a-l <pascal.dihe@cismet.de>
2019-08-21 11:09:10 +0200

    #2 this seems to work

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,8 +1,3 @@
-var path = require('path');
-
 module.exports = {
-    skipComponentsWithoutExample: true,
-    require: [
-      path.resolve(__dirname, 'styleguide/setup.js')
-    ]
-  }
+    skipComponentsWithoutExample: true
+}
