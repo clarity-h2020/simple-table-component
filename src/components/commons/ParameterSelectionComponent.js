36aa500d450b1bbe0104c6ab959520b2696f3576
p-a-s-c-a-l <pascal.dihe@cismet.de>
2020-07-29 11:45:48 +0200

    #18 increase rownum

--- src/components/commons/ParameterSelectionComponent.js
+++ src/components/commons/ParameterSelectionComponent.js
@@ -212,7 +212,7 @@ ParameterSelectionComponent.defaultProps = {
   columns: '',
   emikatParameters: {
     emikatStudyId: undefined,
-    rownum: 1000,
+    rownum: 2500,
     dataFormat: EMIKATHelpers.DATA_FORMAT_VALUES[0],
     studyVariant: EMIKATHelpers.STUDY_VARIANT_VALUES[0], // BASELINE
     timePeriod: EMIKATHelpers.TIME_PERIOD_VALUES[0],
